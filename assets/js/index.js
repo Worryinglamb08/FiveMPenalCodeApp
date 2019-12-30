9e1418397db022c857bee951b6806eb680e4d2b5
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2019-12-30 17:35:52 +0100

    added more stuff

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,19 @@
 function myFunction() {
-  var input, filter, ul, li, a, i, txtValue;
+  let input, filter, ul, li, a, i, txtValue;
   input = document.getElementById("searchBar");
   filter = input.value.toUpperCase();
   ul = document.getElementById("myUL");
+  const con = document.getElementById("container");
   li = ul.getElementsByTagName("li");
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
+      document.getElementById("nothingFound").style.display = "none";
     } else {
+      document.getElementById("nothingFound").style.display = "block";
       li[i].style.display = "none";
     }
   }
-  document.getElementById("container").style.position = "absolute";
-  document.getElementById("container").style.top = "30%";
 }
